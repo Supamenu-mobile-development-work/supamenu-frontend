55a0fac38e178a35ae00cc260364c3535d0387fc
Bwiza-Nina <ninamyles2020@gmail.com>
2023-05-20 11:58:26 +0200

    Finished the home page

--- Supamenu/App.js
+++ Supamenu/App.js
@@ -1,15 +1,31 @@
 import * as React from 'react';
-import {View,Text } from 'react-native';
+import {View,Text,StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';;
 
 function HomeScreen(){
   return (
-    <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>SupaMenu</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Supa<Text style={styles.menu}>Menu</Text></Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: '#F7941D',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  menu: {
+    color: 'white',
+  }
+})
 
 const Stack = createNativeStackNavigator();
 
